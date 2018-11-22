6734afe782d27d480bd65c99ee4a1be285d37e7b
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 01:29:41 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,13 @@
 
 def levitation_quiz
-	#your code here
+	
+	loop do
+	  puts "What is the spell that enacts levitation?"
+	  
+	  spell = gets.chomp
+	 break if spell == "Wingardium Leviosa"
+	 
+	end
 end
 
 
